598fe987247a800201a3fb023afeb36add4230b3
Ben <benjamin.chuffart01@gmail.com>
2023-02-24 15:02:18 +0100

    Tentative 2 de création de Profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,35 +14,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/myProfile", name="myProfile")
+     * @Route("/Profile/{id}", name="Profile")
      */
-    public function showMyProfile(UserRepository $userRepo): Response
+    public function showMyProfile(int $id, UserRepository $userRepository): Response
     {
-        $user =$userRepo;
-
-        return $this->render('user/myProfile.html.twig', [
-        ]);
-    }
-
-    /**
-     * @Route("/modifyProfile", name="modifyProfile")
-     */
-    public function modifyMyProfile(): Response
-    {
-        //todo: aller chercher le profil en BDD
-
-        return $this->render('user/modifyMyProfile.html.twig', [
-        ]);
-    }
-
-    /**
-     * @Route("/userProfile", name="userProfile")
-     */
-    public function userProfile(): Response
-    {
-        //todo: aller chercher le profil en BDD
+        $userProfile = $userRepository->find($id);
 
         return $this->render('user/userProfile.html.twig', [
+            "user" => $userProfile
+        ]);
+    }
+
+    /**
+     * @Route("/modifyProfile/{id}", name="modifyProfile")
+     */
+    public function editMyProfile(int $id,Request $request,EntityManagerInterface $entityManager): Response
+    {
+        $editForm = $this->createForm(User::class);
+
+        $editForm->handleRequest($request);
+        if($editForm->isSubmitted() && $editForm->isValid()){
+            $user = $editForm->getData();
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Profile updated !');
+
+            return $this->redirectToRoute('users_Profile');
+        }
+
+        return $this->render('editProfile.html.twig', [
+            'editForm' => $editForm->createView()
         ]);
     }
 }
