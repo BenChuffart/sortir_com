4cec74e6d7c0ec1472ea9eebc4833edbd4e9b65f
ThibRoch <121017868+ThibRoch@users.noreply.github.com>
2023-02-24 17:31:56 +0100

    Create Filters.php

---
+++ src/Data/Filters.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Data;
+
+class Filters
+{
+    /**
+     * @var string
+     */
+    public $campus ='';
+
+    /**
+    * @var string
+    */
+    public $nameTrip = '';
+
+    /**
+     * @var string
+     */
+    public $startDateTime;
+
+    /**
+     * @var string
+     */
+    public $deadline;
+
+    /**
+     * @var boolean
+     */
+    public $trips = false;
+}
