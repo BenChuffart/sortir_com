d0fdcb13271fdff91bb2d25107f1c6170c1c212e
ThibRoch <121017868+ThibRoch@users.noreply.github.com>
2023-02-27 16:58:09 +0100

    Update TripRepository.php

--- src/Repository/TripRepository.php
+++ src/Repository/TripRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Trip;
+use App\Entity\User;
+use App\Data\Filters;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -31,21 +34,82 @@ class TripRepository extends ServiceEntityRepository
         }
     }
 
-    /*public function findTrip()
+    // FILTRES TOUT LES TRIPS EN LIEN DE LA RECHERCHE 
+    public function findTrip(Filters $filters,User $user) : array{
+
+    
+        $query = $this -> createQueryBuilder('s');
+                        //-> addSelect('','s');
+                        //-> Join('s.name','n');
+       // $query -> addOrderBy('s.startDateTime','DESC');
+
+       if(!empty($filters -> campus))
+       {
+            $query = $query 
+                ->andWhere('s.campus = :campus')
+                ->setParameter('campus', $filters -> campus);
+       }
+       if(!empty($filters -> nameTrip))
+       {
+            $query = $query 
+                ->andWhere('s.name LIKE :nameTrip')
+                ->setParameter('nameTrip',"%{ $filters -> nameTrip}%");
+       }
+       if(!empty($filters -> startDateTime))
+       {
+            $query = $query 
+                ->andWhere('s.startDateTime >= :startDateTime')
+                ->setParameter('startDateTime', $filters -> startDateTime);
+       }
+       if(!empty($filters -> deadline))
+       {
+            $query = $query 
+                ->andWhere('s.deadline <= :deadline')
+                ->setParameter('deadline', $filters -> deadline);
+       }
+       if($filters -> tripsOrganized)
+       {
+            $query = $query 
+                ->andWhere('s.creator = :creator')
+                ->setParameter('creator', $user);
+       }
+       if($filters -> tripsRegisted)
+       {
+            $query = $query 
+                ->andWhere(':user MEMBER OF s.trips')
+                ->setParameter('tripsRegisted',"%{ $filters -> tripsRegisted}%");
+       }
+       if($filters -> tripsNotRegisted)
+       {
+            $query = $query 
+                ->andWhere('s.tripsNotRegisted = :tripsNotRegisted')
+                ->setParameter('tripsNotRegisted',"%{ $filters -> tripsNotRegisted}%");
+       }
+       if($filters -> tripsPassed)
+       {
+            $query = $query 
+                ->andWhere('s.tripsPassed = :tripsPassed')
+                ->setParameter('tripsPassed',"%{ $filters -> tripsPassed}%");
+       }
+       
+       
+       
+        //$paginator = new Paginator($query);
+        return $query ->getQuery() -> getResult();
+
+    }
+
+    //FILTRES LES TRIPS EN FONCTION DE LA DATE
+   /* public function dateInterval(Filters $filters)
     {
-        $queryBuilder = $this -> createQueryBuilder('s');
-        $queryBuilder -> leftJoin('s.name','snam')
-            -> addSelect('snam');
-
-        $queryBuilder -> andWhere('s.maxRegistration < 20');
-        $queryBuilder -> addOrderBy('s.startDateTime','DESC');
-        $query =$queryBuilder ->getQuery();
-        $query->setMaxResults(30);
-
-        $paginator = new Paginator($query);
-        return $paginator;
-
+        $query = $this  -> findTrip($filters)
+                        -> andWhere('s.startDateTime >= :startDateTime')
+                        -> andWhere('s.deadline <= :deadline')
+                        -> getQuery();
+        return $query;
     }*/
+
+
 
     public function remove(Trip $entity, bool $flush = false): void
     {
