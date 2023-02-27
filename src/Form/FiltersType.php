13bf1aaee999b684586c3cd7eb30ad7b12cc4f30
ThibRoch <121017868+ThibRoch@users.noreply.github.com>
2023-02-27 15:54:03 +0100

    Update FiltersType.php

--- src/Form/FiltersType.php
+++ src/Form/FiltersType.php
@@ -2,11 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Data\Filters;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,31 +17,43 @@ class FiltersType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('campus', ChoiceType::class,[
+            ->add('campus', EntityType::class,[
                 'label' => 'Campus',
-                'choices' => [
-                    'Nantes' => 'Nantes',
-                    'Rennes' => 'Rennes',
-                    'Niort' => 'Niort'
-                ],
-                'multiple' => false
+                'class' => Campus::class,
+                'required' => false,
+                'choice_label' => 'name',
             ])
             ->add('nameTrip', TextType::class,[
                 'label' => 'Name Trip',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'Search'
                 ]
             ])
             ->add('startDateTime', DateType::class,[
                 'html5' =>true,
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'required' => false
             ])
             ->add('deadline', DateType::class,[
                 'html5' =>true,
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'required' => false
             ])
-            ->add('trips', CheckboxType::class, [
-                'label' => 'Trips where',
+            ->add('tripsOrganized', CheckboxType::class, [
+                'label' => 'Trips where I organized',
+                'required' => false,
+            ])
+            ->add('tripsRegisted', CheckboxType::class, [
+                'label' => 'Trips where I registed',
+                'required' => false,
+            ])
+            ->add('tripsNotRegisted', CheckboxType::class, [
+                'label' => 'Trips wherec I not registed',
+                'required' => false,
+            ])
+            ->add('tripsPassed', CheckboxType::class, [
+                'label' => 'Trips passed',
                 'required' => false,
             ])
             
