91e1b2fe869bfdb64cd92b62368fdd1267f2dede
Ben <benjamin.chuffart01@gmail.com>
2023-03-02 14:54:43 +0100

    Résolutions "trip->trips" dans les annotations

--- src/Entity/Trip.php
+++ src/Entity/Trip.php
@@ -54,7 +54,7 @@ class Trip
     private $informations;
 
     /**
-     * @ORM\ManyToMany(targetEntity=User::class, mappedBy="trip")
+     * @ORM\ManyToMany(targetEntity=User::class, mappedBy="trips")
      */
     private $users;
 
@@ -65,19 +65,19 @@ class Trip
     private $creator;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Status::class, inversedBy="trip")
+     * @ORM\ManyToOne(targetEntity=Status::class, inversedBy="trips")
      * @ORM\JoinColumn(nullable=false)
      */
     private $status;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Place::class, inversedBy="trip")
+     * @ORM\ManyToOne(targetEntity=Place::class, inversedBy="trips")
      * @ORM\JoinColumn(nullable=false)
      */
     private $place;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Campus::class, inversedBy="trip")
+     * @ORM\ManyToOne(targetEntity=Campus::class, inversedBy="trips")
      * @ORM\JoinColumn(nullable=false)
      */
     private $campus;
